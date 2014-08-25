33448f2c3278e3b1bbe8fd431e47ce8a88e0568f
“Jen <mpjeno@gmail.com>
2014-08-25 15:36:38 -0500

    Assignment completed through question #2

--- day1/Sum.rb
+++ day1/Sum.rb
@@ -3,7 +3,12 @@ require 'pry'
 module Sum
   
   def self.add(numbers)
-    numbers
+    nums = numbers.split(', ')
+    sum = 0
+    nums.each do |n|
+      sum = sum + n.to_i
+    end
+    sum
   end
   
 end
--- day1/spec/lib/sum_spec.rb
+++ day1/spec/lib/sum_spec.rb
@@ -2,19 +2,24 @@ require "spec_helper"
 
 describe Sum do
     
-  it "should return an input, if given only one input" do
-    num = 2
-    expect(Sum.add(2)).to eq(2)
+  it "should return the sum of two numbers, input as a string" do
+    nums = "1, 2"
+    expect(Sum.add(nums)).to eq(3)
   end
   
-  it "should return the sum of two numbers, input as a string"
+  it "should return one number, input as a string" do
+    num = "1"
+    expect(Sum.add(num)).to eq(1)
+  end
+
+  it "should return zero if no numbers are included in the string" do
+    num = ""
+    expect(Sum.add(num)).to eq(0)
+  end
   
-  it "should return the sum of multiple numbers, input as a string"
+  it "should return the sum of any set of numbers, input as a string" do
+    num = "1, 5, 48, 356, 7, 3, 9, 11000, 2"
+    expect(Sum.add(num)).to eq(11431)
+  end
 
-
-  # it "should sum a given plant's yields" do
-  #   a = Plant.create({name: "apple", fruitbearing: 0})
-  #   b = Yield.create({plant_id: 1, number: 20})
-  #   expect(Plant.find(1).total).to eq(20)
-  # end
 end
