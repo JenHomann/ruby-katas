26ecf1c35560e4e9e43f3bd851db963d466b1daf
“Jen <mpjeno@gmail.com>
2014-08-26 21:46:57 -0500

    Broke functionality into specific methods.
    Wrote successful tests for each method.

--- day2/main.rb
+++ day2/main.rb
@@ -19,10 +19,67 @@ module GiftExchange
   #
   # Returns a Hash with each member's name as a key and the person to whom they should give a gift as the value
   def self.assign_gift_givers(members)
-    pairings = members.shuffle.each_slice(2).to_a #randomizes the list, separates into pairs and returns an Array of Arrays, each with a pair of names
-    h = Hash[pairings] #creates a Hash of the pairs and assigns it to a local variable, "h"
-    i = h.invert #returns the inverse of "h" Hash and assigns it to a local variable, "i"
-    final_pairings = h.merge(i) #merges the "h" Hash and "i" Hash into one
+    pairings = self.split_into_pairs(members)
+    pairing_hash = self.array_to_hash(pairings)
+    inverted_hash = self.invert_hash(pairing_hash)
+    final_pairings = self.merge_hashes(pairing_hash, inverted_hash)
+  end
+  
+  # Randomize the list and separate into pairs
+  # 
+  # members - Array containing member names
+  #
+  # Examples
+  #
+  #   split_into_pairs(members)
+  #   # => [["Sumeet Jain", "Beth Haubert"], ["Sachin Jain", "Suneel Jain"], ... etc.]
+  #
+  # Returns an Array of Arrays of matched pairs
+  def self.split_into_pairs(members)
+    members.shuffle.each_slice(2).to_a
+  end
+  
+  # Converts the pairings Array into a Hash
+  #
+  # pairings - an Array of Arrays of matched pairs (computed via self.split_into_pairs)
+  #
+  # Examples
+  #
+  #   array_to_hash(pairings)
+  #   # => {"Sumeet Jain" => "Prisha Gupta", "Beth Haubert" => "Sachin Jain", etc ...}
+  #
+  # Returns a Hash with the matched pairs
+  def self.array_to_hash(pairings)
+    Hash[pairings]
+  end
+  
+  # Inverts the pairing Hash
+  #
+  # pairing_hash - a Hash of matched pairs
+  #
+  # Examples
+  #
+  #   invert_hash(pairing_hash)
+  #   # => {"Sachin Jain" => "Beth Haubert", "Prisha Gupta" => "Sumeet Jain" etc ...}
+  #
+  # Returns a Hash with the matched pairs inverted from the input
+  def self.invert_hash(pairing_hash)
+    pairing_hash.invert
+  end
+  
+  # Merges the pairings_hash and the inverted hash
+  #
+  # pairing_hash - a Hash of matched pairs
+  # inverted_hash - pairing_hash inverted
+  #
+  # Examples
+  #
+  #   mergh_hashes(pairing_hash, inverted_hash)
+  #   # => {"Sumeet Jain" => "Prisha Gupta", "Beth Haubert" => "Sachin Jain", "Sachin Jain" => "Beth Haubert", "Prisha Gupta" => "Sumeet Jain", etc ...}
+  #
+  # Returns a merged Hash with each member assigned to a key and each member assigned to a value
+  def self.merge_hashes(pairing_hash, inverted_hash)
+    pairing_hash.merge(inverted_hash)
   end
   
   #Only assign members to others with different last names
---
+++ day2/spec/lib/main_spec.rb
@@ -0,0 +1,31 @@
+require "spec_helper"
+
+describe GiftExchange do
+  
+  it "should return pairings as a Hash" do
+    members = ["Sumeet Jain", "Beth Haubert"]
+    expect(GiftExchange.assign_gift_givers(members)).to be_a(Hash)
+  end
+  
+  it "should split members into pairs and return an Array" do
+    members = ["A", "B"]
+    expect(GiftExchange.split_into_pairs(members)).to be_an(Array)
+  end
+  
+  it "should convert the pairing Array into a Hash" do
+    pairings = [["A", "B"], ["C", "D"]]
+    expect(GiftExchange.array_to_hash(pairings)).to be_a(Hash)
+  end
+    
+  it "should invert the pairing_hash Hash" do
+    pairing_hash = {"Sumeet Jain" => "Beth Haubert"}
+    expect(GiftExchange.invert_hash(pairing_hash)).to eq({"Beth Haubert" => "Sumeet Jain"})
+  end
+  
+  it "should merge the pairing_hash and inverted hash" do
+    pairing_hash = {"Sumeet Jain" => "Beth Haubert"}
+    inverted_hash = {"Beth Haubert" => "Sumeet Jain"}
+    expect(GiftExchange.merge_hashes(pairing_hash, inverted_hash)).to eq({"Sumeet Jain" => "Beth Haubert", "Beth Haubert" => "Sumeet Jain"})
+  end
+  
+end
